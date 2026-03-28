be69a055d6769b3156e4b88543890cea836b7921
daviarendreis10-cpu <daviarendreis10@gmail.com>
2026-03-28 18:52:40 -0300

    melhorias

--- script.js
+++ script.js
@@ -3,46 +3,77 @@ const tarefa = document.getElementsByTagName('input')[0];
 const lista = document.getElementById('lista');
 const contador = document.getElementById('contador');
 
-adicionar.addEventListener('click', adicionarTarefa);
+adicionar.addEventListener('click', juntar);
+
+
+
+function verificarEnter(e) {
+    if (e.key === 'Enter') {
+        juntar();
+    }
+};
+tarefa.addEventListener('keypress', verificarEnter);
 
 adicionar.disabled = true;
 
-
-tarefa.addEventListener('input', () => {
+function verificarInput() {
     if (tarefa.value.trim() !== "") {
         adicionar.disabled = false;
         return true;
     }else {
-        alert("Por favor, digite uma tarefa válida.");
         adicionar.disabled = true;
         return false;
     }
-});
+};
+
+tarefa.addEventListener('input', verificarInput);
 
 function atualizarContador() {
     contador.textContent = `Total de tarefas: ${lista.children.length}`;
 }
 
-function adicionarTarefa() {
-    if (tarefa.value.trim() === "") { return; }
+function criarLi() {
+    if (tarefa.value.trim() === "") {
+         return; }
 
     const li = document.createElement('li');
     li.textContent = tarefa.value;
+    return li;
 
-    const remover = document.createElement('button');
-    remover.textContent = 'Remover';
-    remover.addEventListener('click', () => {
-        li.remove();
-        atualizarContador();
-    });
+}
 
-    li.appendChild(remover);
+function criarBotaoConcluida(li) {
+
+
+    const concluir = document.createElement('button');
+    concluir.textContent = '⏳';
+    concluir.addEventListener('click', () => {
+        li.textContent = `✅ ${li.textContent}`;
+        li.textContent = li.textContent.replace('⏳', '');
+        concluir.remove();
+        const remover = document.createElement('button');
+        remover.textContent = '❌';
+        remover.style.backgroundColor = '#f8110041';
+        remover.addEventListener('click', () => {
+            li.remove();
+            atualizarContador();
+    });        li.appendChild(remover);
+    }); 
+    return concluir;
+}
+
+
+
+function juntar(li, concluir) {
+    li = criarLi();
+    concluir = criarBotaoConcluida(li);
+
+    li.appendChild(concluir);
+
     lista.appendChild(li);
         tarefa.value = "";
         adicionar.disabled = true;
     atualizarContador();
 
+
 }
-
-
-        
